b60c64bd565179999a99fbab34891a7c1d0d39e7
swaciox <zqmateusz@gmail.com>
2017-01-04 19:38:23 +0100

    New changes

--- app/src/main/java/com/example/verunex/helpmate/ServiceUserProfile.java
+++ app/src/main/java/com/example/verunex/helpmate/ServiceUserProfile.java
@@ -1,13 +1,202 @@
 package com.example.verunex.helpmate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ServiceUserProfile extends AppCompatActivity {
+
+    private ImageView userImage;
+    private TextView userName, userCategory;
+
+    //Firebase
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseReference;
+
+    //Categories choice
+    private TextView userSubCategories;
+    private Button editSubCategories;
+
+    //contact
+    private TextView userNumber, userNumberCell, userEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile_services);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        initControl();
+
+        userOldData();
+
+        userSubCategoriesChoice();
+
+
+    }
+
+    private void initControl(){
+        //userOldData
+        userImage = (ImageView) findViewById(R.id.user_image);
+        userName = (TextView) findViewById(R.id.user_name);
+        userCategory = (TextView) findViewById(R.id.user_category);
+
+        //categories
+        userSubCategories = (TextView)findViewById(R.id.userCategories);
+        editSubCategories = (Button)findViewById(R.id.EditCategories);
+
+        //contact
+        userNumber = (TextView)findViewById(R.id.number);
+        userNumberCell = (TextView)findViewById(R.id.numberSms);
+        userEmail = (TextView)findViewById(R.id.userEmail);
+
+
+    }
+
+    private void userOldData(){
+
+        String id_key = mFirebaseAuth.getCurrentUser().getUid();
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("UserProfile").child(id_key);
+
+        DatabaseReference userData = mDatabaseReference;
+
+        userData.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("name").getValue().toString();
+                userName.setText(name);
+
+                String user_image_uri = dataSnapshot.child("user_image").getValue().toString();
+                if(user_image_uri.isEmpty()){
+                    userImage.setImageResource(R.drawable.person);
+                }else{
+                    Log.v ("user_image uri",user_image_uri);
+                    Picasso.with(getBaseContext()).load(user_image_uri).into(userImage);
+                }
+
+                //contact
+                String number = dataSnapshot.child("number").getValue().toString();
+                String email = dataSnapshot.child("email").getValue().toString();
+
+                userNumberCell.setText(number);
+                userNumber.setText(number);
+                userEmail.setText(email);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void userSubCategoriesChoice()
+    {
+
+
+        editSubCategories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), CategoriesChoice.class);
+                startActivity(i);
+            }
+        });
+
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String temp;
+                String temp2;
+                String desc;
+                String categories = "";
+
+                List<String> category = new ArrayList<String>();
+                int position = 0;
+
+                for (int i = 1; i <=23; i++){
+                    temp = "sub"+i;
+
+                    temp2 = dataSnapshot.child("categories").child(temp).child("state").getValue().toString();
+                    if(temp2.equals("true")){
+                        desc = dataSnapshot.child("categories").child(temp).child("description").getValue().toString();
+                        categories = categories + "\n-" + desc;
+
+                        if(temp.equals("sub1") || temp.equals("sub2")){
+                            category.add("Hydraulika");
+                        }else if (temp.equals("sub3") || temp.equals("sub4")){
+                            category.add("Elektryka");
+                        }else if (temp.equals("sub5") || temp.equals("sub6") || temp.equals("sub7")){
+                            category.add("Pomoc domowa");
+                        } else if(temp.equals("sub8")|| temp.equals("sub9")|| temp.equals("sub10")|| temp.equals("sub11")){
+                            category.add("Opieka");
+                        } else if (temp.equals("sub12")){
+                            category.add("Korepetycje");
+                        } else if (temp.equals("sub13")|| temp.equals("sub14")|| temp.equals("sub15")){
+                            category.add("Ogrodnictwo");
+                        } else if (temp.equals("sub16")|| temp.equals("sub17")|| temp.equals("sub18")|| temp.equals("sub19")){
+                            category.add("Naprawa urządzeń");
+                        } else if (temp.equals("sub20")|| temp.equals("sub21")|| temp.equals("sub22")|| temp.equals("sub23")){
+                            category.add("Remonty");
+                        }
+                    }
+                }
+
+                List<String> finallist = new ArrayList<String>();
+
+                //usuniecie duplikatow
+                Set<String> mySet = new HashSet<String>(category);
+                for(String tempList : mySet ){
+                    Log.v ("templist", tempList);
+                    finallist.add(tempList);
+                }
+
+                String filterCategory = "";
+                for(int i = 0; i < finallist.size(); i++){
+                    filterCategory = filterCategory +finallist.get(i)+ ", ";
+                    Log.v("filter ", filterCategory);
+                }
+
+
+                userCategory.setText(deleteLastChar(filterCategory));
+                userSubCategories.setText(categories);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private String deleteLastChar(String s ){
+
+        String temp = "";
+        if (!s.isEmpty()){
+            temp = s.substring(0,s.length()-2);
+        }
+        return temp;
     }
 }
